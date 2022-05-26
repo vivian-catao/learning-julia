8abf53e080a2da9f656ad2d99264d0574cf30dcb
Vivian Catão <59140869+vivian-catao@users.noreply.github.com>
2022-05-26 18:00:41 -0300

    Create armstrong-number.jl

---
+++ armstrong-number.jl
@@ -0,0 +1,26 @@
+println("Digite um numero")
+n = readline()
+num = parse(Int,n)
+cont = length(n)
+x = digits(num)
+soma=0
+
+for i = 1:cont
+    global soma
+    global x
+    println("$(x[i])^$(cont) ")
+    soma = soma + (x[i]^cont)
+
+end 
+
+    println("O número é: $n")
+    println("A soma é: $soma") 
+
+
+if  (soma == n)
+    println("O numero $(n) é um número de Armstrong")
+
+else 
+    println("O numero $(n) não é um número de Armstrong")
+end
+println("\n")
