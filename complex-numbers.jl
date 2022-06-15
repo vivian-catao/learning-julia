ba4254beff3fcce3abde9c1591b0ae104319c65e
Vivian Catão <vivian.tenorio@embedded.ufcg.edu.br>
2022-06-15 16:25:01 -0300

    função multiplicação

--- complex-numbers.jl
+++ complex-numbers.jl
@@ -3,11 +3,13 @@ struct ComplexNumber <: Number
     imag::Real
     ComplexNumber(real, imag) = new(real, imag)
 end
+    function mult(a::ComplexNumber, b::ComplexNumber)
+        return ComplexNumber(a.real * b. real - a.imag * b.imag, a.imag * b.real + a.real * b.imag)
+    end
+    function soma(a::ComplexNumber, b::ComplexNumber)
+        return ComplexNumber(a.real + b.real, a.imag + b.imag)
+    end
 
-    # function soma(a::ComplexNumber, b::ComplexNumber)
-    #     return ComplexNumber(a.real + b.real, a.imag + b.imag)
-    # end
-
-    # function sub(a::ComplexNumber, b::ComplexNumber)
-    #     return ComplexNumber(a.real - b.real, a.imag - b.imag)
-    # end
+    function sub(a::ComplexNumber, b::ComplexNumber)
+        return ComplexNumber(a.real - b.real, a.imag - b.imag)
+    end
