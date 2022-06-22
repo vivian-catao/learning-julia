aecd452defe94f447e52ae285763d279511fae19
Vivian Catão <vivian.tenorio@embedded.ufcg.edu.br>
2022-06-22 17:18:09 -0300

    Correção das funções:soma, sub e mult

--- complex-numbers.jl
+++ complex-numbers.jl
@@ -1,15 +1,14 @@
+import Base: +, -, *, /, abs, conj, real, imag
+
 struct ComplexNumber <: Number
     real::Real
     imag::Real
     ComplexNumber(real, imag) = new(real, imag)
 end
-    function mult(a::ComplexNumber, b::ComplexNumber)
-        return ComplexNumber(a.real * b. real - a.imag * b.imag, a.imag * b.real + a.real * b.imag)
-    end
-    function soma(a::ComplexNumber, b::ComplexNumber)
-        return ComplexNumber(a.real + b.real, a.imag + b.imag)
-    end
 
-    function sub(a::ComplexNumber, b::ComplexNumber)
-        return ComplexNumber(a.real - b.real, a.imag - b.imag)
-    end
+    +(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real + b.real, a.imag + b.imag)
+
+    -(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real - b.real, a.imag - b.imag)
+
+    *(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real*b.real  - b.imag*a.imag , a.real*b.imag + a.imag*b.real)
+
