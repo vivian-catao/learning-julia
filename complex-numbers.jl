85a507ca4cd9fe4c3dd98f80660ebaf5c600720b
Vivian Catão <vivian.tenorio@embedded.ufcg.edu.br>
2022-06-22 17:54:30 -0300

    Adicionando as funçõesde div, modulo, conjugado, real e imaginario

--- complex-numbers.jl
+++ complex-numbers.jl
@@ -11,3 +11,13 @@ end
     -(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real - b.real, a.imag - b.imag)
 
     *(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real*b.real  - b.imag*a.imag , a.real*b.imag + a.imag*b.real)
+
+    /(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real*b.real  - b.imag*a.imag , a.real*b.imag + a.imag*b.real)
+
+    abs(a::ComplexNumber) = sqrt(a.real^2 + a.imag^2)
+
+    conj(a::ComplexNumber) = ComplexNumber(a.real, - a.imag)
+
+    real(a::ComplexNumber) = a.real
+
+    imag(a::ComplexNumber) = a.imag
